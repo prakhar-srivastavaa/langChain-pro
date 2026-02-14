12a6086638ea2d1074ca736441961e5d75449892
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-14 11:26:46 +0530

    SystemMessage, HumanMessage, AIMessage

--- 4.prompts-In-langChain/Project/message.py
+++ 4.prompts-In-langChain/Project/message.py
@@ -1,6 +1,7 @@
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 import os
+
 # Set huggingface cache home
 os.environ["HF_HOME"] = "Z:/huggingface_cache"
 
@@ -8,19 +9,25 @@ model_name = "LGAI-EXAONE/EXAONE-4.0-1.2B"
 
 print("Loading model and tokenizer...")
 
-# Load the model and tokenizer
-model = ChatHuggingFace.from_model_id(
+# Load the model using HuggingFacePipeline
+llm = HuggingFacePipeline.from_model_id(
     model_id=model_name,
     task="text-generation",
-    device_map="auto"
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
 )
 
-messages=[
+# Wrap it in ChatHuggingFace
+model = ChatHuggingFace(llm=llm)
+
+messages = [
     SystemMessage(content="You are a helpful assistant."),
     HumanMessage(content="Tell me about langchain."),
 ]
 
-result=model.invoke(messages)
-messages.append(AIMessage(result.content))
+result = model.invoke(messages)
+messages.append(AIMessage(content=result.content))
 
 print(messages)
