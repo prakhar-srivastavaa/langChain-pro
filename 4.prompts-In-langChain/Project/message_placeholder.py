1d2223afd928124ebf0b17fab3faa945cf0a7216
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-16 20:23:03 +0530

    sending layers of chat history and prev chat history

---
+++ 4.prompts-In-langChain/Project/message_placeholder.py
@@ -0,0 +1,19 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+#chat template
+chat_template= ChatPromptTemplate([
+    ('system', 'you are a helpful coustomer support agent'),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('human', '{query}')
+])
+
+#load chat history
+chat_history = []
+print("1")
+with open('z:/langChain/4.prompts-In-langChain/Project/chat_history.txt') as f:
+    chat_history.extend(f.readlines())
+    print("3")
+print(chat_history)
+
+#create prompt
+final_prompt= chat_template.invoke({'chat_history':chat_history, 'query':"where is my refund"})
