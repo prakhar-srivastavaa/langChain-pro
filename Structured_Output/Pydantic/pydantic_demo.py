2734b23cee375402a3e1865e889c2a9aadf6ec81
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-19 21:49:57 +0530

    optional,pydantic-basemodel,emailstr, fields

--- Structured_Output/Pydantic/pydantic_demo.py
+++ Structured_Output/Pydantic/pydantic_demo.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr, Field
 from typing import Optional
 
 class Student(BaseModel):
     name: str
     email: Optional[EmailStr]= None
+    CGPA: float=Field(gt=0,lt=10, default=5, description="A decimal value of total scored marks as CGPA out of 10")
 
-
-new_student={'name':'Prakhar','email':'abc@gmail.com'}
+new_student={'name':'Prakhar','email':'abc@gmail.com', 'CGPA':8.07}
 new_number={'name':'Srivastava'}
 student= Student(**new_student)
 number= Student(**new_number)
