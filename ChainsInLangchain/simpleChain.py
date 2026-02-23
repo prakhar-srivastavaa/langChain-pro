741c05028790b1441da9ec678abb2c3a15602793
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-23 17:28:14 +0530

    simple chain

---
+++ ChainsInLangchain/simpleChain.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENROUTER_API_KEY")
+
+prompt= PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+model = ChatOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+    model="arcee-ai/trinity-large-preview:free",
+    temperature=0.5,
+    max_tokens=200,
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+response = chain.invoke({"topic": "langchain"})
+print(response)
+
+chain.get_graph().print_ascii()
