ffd0cbdcafd8be7bcd3b0d07ac4f187308e10632
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-25 11:53:39 +0530

    passthrough, joke and explanation

---
+++ 8.RunnablesInLangchain/3.runnablePassThrough-jokegenerator.py
@@ -0,0 +1,47 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence , RunnableParallel, RunnablePassthrough
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+apikey=os.getenv("OPENROUTER_API_KEY")
+
+model = ChatOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=apikey,
+    model="arcee-ai/trinity-large-preview:free",
+    temperature=0.5,
+    max_tokens=512
+)
+prompt1 = PromptTemplate(
+    template="write a joke about {topic}",
+    input_variables=["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template="explain the folloeing joke {text}",
+    input_variables=["text"]
+)
+
+parser = StrOutputParser()
+
+jokeGeneratorchain = RunnableSequence(prompt1, model, parser)
+
+parallelChain = RunnableParallel({
+    "joke": RunnablePassthrough(),
+    "explanation": RunnableSequence(prompt2, model, parser)
+})
+
+finalChain= RunnableSequence(jokeGeneratorchain,parallelChain)
+
+result = finalChain.invoke({"topic":"AI"})
+
+print(result)
+
+finalChain.get_graph().print_ascii()
+
+
+
