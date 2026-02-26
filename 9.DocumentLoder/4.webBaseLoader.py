f2c2477ea2ac8e1894ff6a662fcc06c5f4a9b5d7
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-26 23:33:52 +0530

    WebBaseLoader

--- 9.DocumentLoder/4.webBaseLoader.py
+++ 9.DocumentLoder/4.webBaseLoader.py
@@ -3,7 +3,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
-import os
+import os 
 load_dotenv()
 
 apikey=os.getenv("OPENROUTER_API_KEY")
