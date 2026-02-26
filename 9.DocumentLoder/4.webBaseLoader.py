f866699382ed06838a04df8a065c126b329e67a8
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-26 18:21:40 +0530

    WebBaseLoader

---
+++ 9.DocumentLoder/4.webBaseLoader.py
@@ -0,0 +1,33 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+apikey=os.getenv("OPENROUTER_API_KEY")
+
+model = ChatOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=apikey,
+    model="arcee-ai/trinity-large-preview:free",
+    temperature=0.5,
+    max_tokens=512
+)
+
+prompt = PromptTemplate(
+    template='Answer the following question \n {question} from the following text - \n {text}',
+    input_variables=['question','text']
+)
+
+parser = StrOutputParser()
+
+url = 'https://github.com/prakhar-srivastavaa'
+loader = WebBaseLoader(url)
+
+docs = loader.load()
+
+chain = prompt | model | parser
+
+print(chain.invoke({'question':'extract details like name eductaion experience and what skills the person holds?', 'text':docs[0].page_content}))
