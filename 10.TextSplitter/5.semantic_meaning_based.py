12989f6a9d4bf73586fff8e0a84be2156c77e020
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-27 14:03:02 +0530

    using standard_deviation of symantic meaning text splitter

---
+++ 10.TextSplitter/5.semantic_meaning_based.py
@@ -0,0 +1,32 @@
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_openai.embeddings import OpenAIEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Configure embeddings to use OpenRouter
+embeddings = OpenAIEmbeddings(
+    model="arcee-ai/trinity-large-preview:free",
+
+    # model="openai/text-embedding-3-small",  # this is chargable pricing is low 
+    openai_api_key=os.getenv("OPENROUTER_API_KEY"),
+    openai_api_base="https://openrouter.ai/api/v1"
+)
+
+text_splitter = SemanticChunker(
+    embeddings, 
+    breakpoint_threshold_type="standard_deviation",
+    breakpoint_threshold_amount=1
+)
+
+sample = """
+Farmers were working hard in the fields, preparing the soil and planting seeds for the next season. The sun was bright, and the air smelled of earth and fresh grass. The Indian Premier League (IPL) is the biggest cricket league in the world. People all over the world watch the matches and cheer for their favourite teams.
+
+
+Terrorism is a big danger to peace and safety. It causes harm to people and creates fear in cities and villages. When such attacks happen, they leave behind pain and sadness. To fight terrorism, we need strong laws, alert security forces, and support from people who care about peace and safety.
+"""
+
+docs = text_splitter.create_documents([sample])
+print(len(docs))
+print(docs)
